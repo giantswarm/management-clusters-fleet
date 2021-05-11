2e76f202630823e673764613dee810adef15d3fc
vol <me@volodymyrstoiko.com>
2021-05-11 12:29:17 +0300

    Add script

---
+++ hack/update-argocd-manifests.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+set -x
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ARGOCD_REPOSITORY="https://github.com/argoproj/argo-cd.git"
+ARGOCD_VERSION=${1:-master}
+SRCROOT="$( CDPATH='' cd -- "$(dirname "$0")/.." && pwd -P )"
+WORK_DIR=$(mktemp -d -t ci-XXXXXXXXXX)
+
+KUSTOMIZE=kustomize
+
+AUTOGENMSG="# This is an auto-generated file. DO NOT EDIT"
+
+IMAGE_NAMESPACE="${IMAGE_NAMESPACE:-quay.io/giantswarm}"
+IMAGE_TAG="${ARGOCD_VERSION:-latest}"
+
+$KUSTOMIZE version
+
+git clone --depth 1 --branch ${ARGOCD_VERSION} ${ARGOCD_REPOSITORY} ${WORK_DIR}
+cd ${WORK_DIR}
+
+cd manifests/base && $KUSTOMIZE edit set image quay.io/argoproj/argocd=${IMAGE_NAMESPACE}/argocd:${IMAGE_TAG} && cd -
+
+sed -i "/dex/d" manifests/base/kustomization.yaml
+
+echo "${AUTOGENMSG}" > "${SRCROOT}/manifests/install.yaml"
+$KUSTOMIZE build "manifests/cluster-install" >> "${SRCROOT}/manifests/install.yaml"
+
+cd -
+rm $WORK_DIR -Rf
