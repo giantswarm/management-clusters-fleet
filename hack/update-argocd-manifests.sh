acc914f765e74b033e9c75173ef25abb094a5364
Paweł Kopiczko <kopiczko@gmail.com>
2021-05-13 16:37:45 +0100

    Restore hack/

--- hack/update-argocd-manifests.sh
+++ hack/update-argocd-manifests.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+set -x
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -15,15 +16,13 @@ AUTOGENMSG="# This is an auto-generated file. DO NOT EDIT"
 IMAGE_NAMESPACE="${IMAGE_NAMESPACE:-quay.io/giantswarm}"
 IMAGE_TAG="${ARGOCD_VERSION:-latest}"
 
-set -x
-
 ${KUSTOMIZE} version
 
-git clone --quiet --depth 1 --branch ${ARGOCD_VERSION} ${ARGOCD_REPOSITORY} ${WORK_DIR}
+git clone --depth 1 --branch ${ARGOCD_VERSION} ${ARGOCD_REPOSITORY} ${WORK_DIR}
 cd ${WORK_DIR}
 
 cd manifests/base
-${KUSTOMIZE} edit set image quay.io/argoproj/argocd=${IMAGE_NAMESPACE}/argocd:${IMAGE_TAG}
+${KUSTOMIZE} edit set image quay.io/argoproj/argocd=${IMAGE_NAMESPACE}/argocd:${IMAGE_TAG} 
 ${KUSTOMIZE} edit set namespace argocd
 cd -
 
@@ -35,3 +34,9 @@ ${KUSTOMIZE} build "manifests/cluster-install" >> "${SRCROOT}/build/argocd/insta
 
 cd ${SRCROOT}
 rm $WORK_DIR -Rf
+
+mkdir -p ${SRCROOT}/build/provider
+for provider in $(ls ${SRCROOT}/manifests/provider); do
+  echo "${AUTOGENMSG}" > "${SRCROOT}/build/provider/${provider}.yaml"
+  ${KUSTOMIZE} build ${SRCROOT}/manifests/provider/${provider} >> "${SRCROOT}/build/provider/${provider}.yaml"
+done
