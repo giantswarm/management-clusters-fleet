4ee92e5d20aa1f55e18211ba6e22a6014b7d0e94
Paweł Kopiczko <pawel@giantswarm.io>
2021-05-26 10:15:15 +0100

    Download kustomize during manifests generation (#27)

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,33 +1,55 @@
 AUTOGENMSG := \# This is an auto-generated file. DO NOT EDIT
 
-KUSTOMIZE := kustomize
-
 MANIFESTS := $(shell find manifests)
 
+KUSTOMIZE := bin/kustomize
+
 .PHONY: all
-all: build/argocd build/provider
+all: manifests/bases/upstream-argocd bootstrap
 
-.PHONY: build/argocd
-build/argocd: ARGOCD_VERSION=v2.0.1
-build/argocd:
+.PHONY: manifests/bases/upstream-argocd
+manifests/bases/upstream-argocd: ARGOCD_VERSION := v2.0.1
+manifests/bases/upstream-argocd: ARGOCD_REPOSITORY := https://github.com/argoproj/argo-cd.git
+manifests/bases/upstream-argocd: WORK_DIR := $(shell mktemp -d -t ci-XXXXXXXXXX)
+manifests/bases/upstream-argocd: $(KUSTOMIZE)
 	@echo "====> $@"
-	./hack/update-argocd-manifests.sh $(ARGOCD_VERSION)
+	git -c advice.detachedHead=false clone --quiet --depth 1 --branch ${ARGOCD_VERSION} ${ARGOCD_REPOSITORY} ${WORK_DIR}
+	mkdir -p manifests/bases/upstream-argocd
+	echo "$(AUTOGENMSG)" > manifests/bases/upstream-argocd/install.yaml
+	$(KUSTOMIZE) build "$(WORK_DIR)/manifests/cluster-install" >> manifests/bases/upstream-argocd/install.yaml
+	rm -rf $(WORK_DIR)
 
-build/provider: $(MANIFESTS)
-	@echo "====> $@"
-	mkdir -p $@
-	@$(MAKE) bootstrap/aws.yaml
-	@$(MAKE) bootstrap/aws-test.yaml
-	@$(MAKE) bootstrap/aws-china.yaml
-	@$(MAKE) bootstrap/aws-china-test.yaml
-	@$(MAKE) bootstrap/azure.yaml
-	@$(MAKE) bootstrap/azure-test.yaml
-	@$(MAKE) bootstrap/kvm.yaml
-	@$(MAKE) bootstrap/kvm-test.yaml
-	@$(MAKE) bootstrap/vmware.yaml
-	@$(MAKE) bootstrap/vmware-test.yaml
+BOOTSRAP_DEPS :=
+BOOTSRAP_DEPS += bootstrap/aws.yaml
+BOOTSRAP_DEPS += bootstrap/aws-test.yaml
+BOOTSRAP_DEPS += bootstrap/aws-china.yaml
+BOOTSRAP_DEPS += bootstrap/aws-china-test.yaml
+BOOTSRAP_DEPS += bootstrap/azure.yaml
+BOOTSRAP_DEPS += bootstrap/azure-test.yaml
+BOOTSRAP_DEPS += bootstrap/kvm.yaml
+BOOTSRAP_DEPS += bootstrap/kvm-test.yaml
+BOOTSRAP_DEPS += bootstrap/vmware.yaml
+BOOTSRAP_DEPS += bootstrap/vmware-test.yaml
+bootstrap: $(BOOTSRAP_DEPS)
 
-bootstrap/%.yaml: $(MANIFESTS)
+bootstrap/%.yaml: $(KUSTOMIZE) $(MANIFESTS)
 	@echo "====> $@"
+	mkdir -p bootstrap
 	echo "$(AUTOGENMSG)" > $@
 	$(KUSTOMIZE) build manifests/provider/$* >> $@
+
+$(KUSTOMIZE): ## Download kustomize locally if necessary.
+	@echo "====> $@"
+	$(call go-get-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v4@v4.1.2)
+
+# go-get-tool will 'go get' any package $2 and install it to $1.
+PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+define go-get-tool
+set -e ;\
+TMP_DIR=$$(mktemp -d) ;\
+cd $$TMP_DIR ;\
+go mod init tmp ;\
+echo "Downloading $(2)" ;\
+GOBIN=$(PROJECT_DIR)/bin go get $(2) ;\
+rm -rf $$TMP_DIR
+endef
