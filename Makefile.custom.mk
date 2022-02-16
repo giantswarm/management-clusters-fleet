827e0a4d325312fd79431776058b207d8c46ffa9
Łukasz Piątkowski <piontec@gmail.com>
2022-02-16 13:35:25 +0100

    add gcp

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -17,15 +17,18 @@ manifests/flux-app:
 	find manifests/provider/ -wholename '*/kustomization.yaml' | grep -v 'charts' | xargs -I{} sed -i "0,/version:/{s/version: .*/version: $(FLUXAPP_VERSION)/g}" {}
 
 BOOTSRAP_DEPS :=
-BOOTSRAP_DEPS += bootstrap/gs-aws/gs-aws.yaml
-BOOTSRAP_DEPS += bootstrap/gs-aws-china/gs-aws-china.yaml
 BOOTSRAP_DEPS += bootstrap/customer-aws/customer-aws.yaml
 BOOTSRAP_DEPS += bootstrap/customer-aws-china/customer-aws-china.yaml
-BOOTSRAP_DEPS += bootstrap/gs-azure/gs-azure.yaml
 BOOTSRAP_DEPS += bootstrap/customer-azure/customer-azure.yaml
-BOOTSRAP_DEPS += bootstrap/gs-kvm/gs-kvm.yaml
+BOOTSRAP_DEPS += bootstrap/customer-gcp/customer-gcp.yaml
+BOOTSRAP_DEPS += bootstrap/customer-openstack/customer-openstack.yaml
 BOOTSRAP_DEPS += bootstrap/customer-kvm/customer-kvm.yaml
 BOOTSRAP_DEPS += bootstrap/customer-openstack/customer-openstack.yaml
+BOOTSRAP_DEPS += bootstrap/gs-aws/gs-aws.yaml
+BOOTSRAP_DEPS += bootstrap/gs-aws-china/gs-aws-china.yaml
+BOOTSRAP_DEPS += bootstrap/gs-azure/gs-azure.yaml
+BOOTSRAP_DEPS += bootstrap/gs-gcp/gs-gcp.yaml
+BOOTSRAP_DEPS += bootstrap/gs-kvm/gs-kvm.yaml
 BOOTSRAP_DEPS += bootstrap/gs-openstack/gs-openstack.yaml
 BOOTSRAP_DEPS += bootstrap/gs-openstack-galaxy/gs-openstack-galaxy.yaml
 BOOTSRAP_DEPS += bootstrap/gs-openstack-gamma/gs-openstack-gamma.yaml
